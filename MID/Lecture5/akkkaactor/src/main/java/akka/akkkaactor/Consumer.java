8553e7a69b2edc2bf643159b1de0481337ba3c78
rcep <recepsigmod@gmail.com>
2017-11-29 12:49:54 +0100

    mid lec 5-6

---
+++ MID/Lecture5/akkkaactor/src/main/java/akka/akkkaactor/Consumer.java
@@ -0,0 +1,12 @@
+package akka.akkkaactor;
+
+import akka.actor.UntypedActor;
+
+public class Consumer  extends UntypedActor {
+	@Override
+    public void onReceive(Object msg) throws Exception {
+        if(msg instanceof Integer) {
+            System.out.println("<<< Receiving & printing " + msg);
+        }
+    }
+}
---
+++ MID/Lecture5/akkkaactor/src/main/java/akka/akkkaactor/Producer.java
@@ -0,0 +1,21 @@
+package akka.akkkaactor;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+public class Producer {
+	public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("e");
+        ActorRef printNumbersConsumer = system.actorOf(Props.create(Consumer.class));
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(">>> Producing & sending a number " +  i);
+            printNumbersConsumer.tell(i, ActorRef.noSender());
+        }
+        
+        system.terminate();
+        System.out.println("===== Finished producing & sending numbers 1 to 10");
+        
+    }
+}
