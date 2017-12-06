26a82b4a7ee9019690cafc468c58dd7626b9730f
Syd Gillani <syd@Syds-MacBook-Pro-3.local>
2017-12-06 10:40:55 +0100

    mid lec

---
+++ MID/Lecture7/2phasecommit/src/org/shared/FakeResource.java
@@ -0,0 +1,10 @@
+package org.shared;
+
+public class FakeResource {
+	public void commit(){
+		System.out.println("\tCOMMITED!");
+	}
+	public void abort(){
+		System.out.println("\tABORTED!");
+	}
+}
